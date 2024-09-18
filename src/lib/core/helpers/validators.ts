1c09bb34ed51a40e83611ceebe8fdb3ab20b9de3
Amos <igefad12@gmail.com>
2024-09-18 18:24:06 +0100

    UPDATE: Todos

--- src/lib/core/helpers/validators.ts
+++
@@ -1,8 +0,0 @@
-// VALIDATORS
-// ==========
-
-// USERNAME VALIDATOR
-export function validateUserName(userName: string): boolean {
-    const regex = /^[a-zA-Z0-9_]{5,}$/;
-    return regex.test(userName);
-}
