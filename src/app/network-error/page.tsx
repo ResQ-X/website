ee2dcdd2fd39e561c0b0d1abc3ae934c2ba70072
Amos <igefad12@gmail.com>
2024-09-19 09:30:27 +0100

    UPDATE: NetworkError

--- src/app/network-error/page.tsx
+++ src/app/network-error/page.tsx
@@ -11,7 +11,7 @@ export default function NetworkError() {
           <h1 className="mb-4 font-['Raleway'] text-[40px] font-bold leading-[46.96px] tracking-[-2%] text-black">
             Oops....
           </h1>
-          <h2 className="mb-[18px] font-['Raleway'] text-[32px] font-semibold leading-[37.57px] tracking-[-2%] text-black">
+          <h2 className="mb-[18px] text-center font-['Raleway'] text-[32px] font-semibold leading-[37.57px] tracking-[-2%] text-black">
             Looks like you're offline.
           </h2>
           <p className="mb-[60px] text-center font-['Raleway'] text-lg font-normal  text-black lg:text-start">
@@ -33,7 +33,7 @@ export default function NetworkError() {
           <Image
             height={214}
             width={214}
-            alt="FAQs Image"
+            alt="Network Error Image"
             src={"/images/network_error_image.png"}
             className="relative max-h-full w-full overflow-hidden object-cover"
           />
