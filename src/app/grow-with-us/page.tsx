3eee177f41d9dd0c0eddf933c0c1bc0e48aebb29
Amos <igefad12@gmail.com>
2024-09-20 21:01:55 +0100

    UPDATE: WhatYouGain

--- src/app/grow-with-us/page.tsx
+++ src/app/grow-with-us/page.tsx
@@ -1,12 +1,14 @@
 import { GrowWithUsHero } from "@/views/grow-with-us/GrowWithUsHero";
 import Faqs from "../faqs/page";
 import { CareerContact } from "@/views/careers/CareerContact";
+import { WhatYouGain } from "@/views/grow-with-us/WhatYouGain";
 
 export default function Page() {
   return (
     <main>
       <div>
         <GrowWithUsHero />
+        <WhatYouGain />
         <CareerContact />
         <Faqs />
       </div>
---
+++ src/views/grow-with-us/WhatYouGain.tsx
@@ -0,0 +1,28 @@
+export const WhatYouGain = () => {
+  return (
+    <div>
+      <div className="h-[576px] w-full bg-[#332414] font-['Raleway'] ">
+        <div className="h-10"></div>
+        <p className="text-center text-[50px] font-semibold leading-[55px] text-white md:text-[64px] md:leading-[75.14px]">
+          Here's What You Gain
+        </p>
+        <p className="mb-6 text-center text-[50px] font-semibold leading-[55px] text-white md:text-[64px] md:leading-[75.14px]">
+          With Us
+        </p>
+      </div>
+      <div className="mx-4 mt-[-330px] flex flex-col items-center rounded-[12px] border-[5px] border-[#736250] bg-white px-6 lg:mx-[50px] lg:flex-row lg:px-[27px] ">
+        <div className="flex flex-col text-center lg:w-3/5 lg:justify-start lg:text-start">
+          <p className="mb-8 text-[50px] font-semibold leading-[55px] text-[#332414] md:text-[64px] md:leading-[75.14px]">
+            Steady Stream of Work, Transparent Earnings
+          </p>
+          <p className="mb-6  text-base font-normal leading-[24px] text-[#333333] ">
+            Access a constant flow of service requests to keep your business
+            busy and profitable. Easily track your earnings through the app and
+            receive payments promptly.
+          </p>
+        </div>
+        <div className="lg:w-2/3"></div>
+      </div>
+    </div>
+  );
+};
