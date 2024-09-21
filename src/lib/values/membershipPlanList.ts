b5bbd37df7de94c3d3e1f088343239b0a8204641
Amos <igefad12@gmail.com>
2024-09-21 16:35:54 +0100

    COMPLETED: Membership

--- src/lib/values/membershipPlanList.ts
+++ src/lib/values/membershipPlanList.ts
@@ -46,24 +46,44 @@ export const quaterlyPlanList: IMembershipPlan[] = [
 
 export const annualPlanList: IMembershipPlan[] = [
   {
-    price: '',
-    type: '',
+    price: '₦39,500',
+    type: 'Premium',
     url: '',
     customClassName: "bg-[#FF725E]",
-    benefitList: [],
+    benefitList: [
+      "Roadside assistance 1/4 mile from your home",
+      "Free car health check",
+      "3 callouts per quarter",
+      "24/7 roadside assistance",
+      "Free recovery up to 5 miles to your preferred destination",
+    ],
   },
   {
-    price: '',
-    type: '',
+    price: '₦26,500',
+    type: 'Extra',
     url: '',
-    customClassName: "bg-[#FF725E]",
-    benefitList: [],
+    paddingTop: '46px',
+    paddingBottom: '113px',
+    customClassName: "[background:linear-gradient(180deg,_#ff8500,_#995000)]",
+    benefitList: [
+      "Roadside assistance 1/4 mile from your home",
+      "Free car health check",
+      "3 callouts per quarter",
+      "24/7 roadside assistance",
+      "Free recovery up to 5 miles to your preferred destination",
+    ],
   },
   {
-    price: '',
-    type: '',
+    price: '₦14,500',
+    type: 'Basic',
     url: '',
-    customClassName: "bg-[#FF725E]",
-    benefitList: [],
+    customClassName: "bg-[#332414]",
+    benefitList: [
+      "Roadside assistance 1/4 mile from your home",
+      "Free car health check",
+      "3 callouts per quarter",
+      "24/7 roadside assistance",
+      "Free recovery up to 5 miles to your preferred destination",
+    ],
   }
 ]
--- src/views/membership/AnnualPlanTab.tsx
+++ src/views/membership/AnnualPlanTab.tsx
@@ -1,7 +1,17 @@
+import { annualPlanList } from "@/lib/values/membershipPlanList";
+import { PricingCard } from "./PricingCard";
+
 export const AnnualPlanTab = () => {
   return (
-    <div>
-      <div></div>
+    <div className="">
+      <div className="mx-4 flex flex-col gap-10 lg:mx-24 lg:mb-[-330px] lg:flex-row lg:items-end lg:justify-center">
+        {annualPlanList.map((plan, index) => (
+          <div key={index}>
+            <PricingCard {...plan} />
+          </div>
+        ))}
+      </div>
+      <div className="display:none hidden h-[427px] w-full bg-[#07016C] lg:block "></div>
     </div>
   );
 };
