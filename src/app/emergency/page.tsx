e2e42baff637fc18cbb5ac97ac06fc52fe7152b4
Eric Alfred <alfrederic371@gmail.com>
2024-11-26 23:59:25 +0100

    update: resq me page

---
+++ src/app/emergency/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { Tabs } from "@/components/ui/tabs";
+import { Metadata } from "next";
+import HeroSection from "@/views/emergency/HeroSection";
+import TabsSection from "@/views/emergency/TabSection";
+import DummyContent from "@/views/emergency/DummyContext";
+// import { AlertTriangle } from "lucide-react";
+
+export const metadata: Metadata = {
+  title: "Rescue me - ResQX | Fast, Reliable And Affordable Roadside Assistance Near You",
+  description: "ResQX Website",
+};
+
+export default function RescueMe() {
+
+
+  return (
+    <div className="min-h-screen px-8 py-32 w-full flex flex-col items-center justify-center">
+      {/* Hero Section */}
+      <HeroSection />
+      {/* Tabs Section */}
+      <TabsSection />
+    </div>
+  );
+}
+
