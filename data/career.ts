75ba5d837cbd775a3a3f29dfec59efddb603f5b4
Eric Alfred <alfrederic371@gmail.com>
2025-04-12 13:21:05 +0100

    update: add new job positions for Product Designer and Personal Assistant to CEO

--- data/career.ts
+++ data/career.ts
@@ -1,29 +1,24 @@
 export const careersData: Career[] = [
-  // {
-  //   id: "wordpress-developer",
-  //   title: "Wordpress Developer",
-  //   experience: "2 Years",
-  //   deadline: "2021-05-08",
-  // },
-  // {
-  //   id: "customer-service-representative",
-  //   title: "Customer Service Representative",
-  //   experience: "1-3 Years",
-  //   deadline: "2025-02-20",
-  // },
-  // {
-  //   id: "growth-marketer",
-  //   title: "Growth Marketer",
-  //   experience: "3-5 Years",
-  //   deadline: "2025-02-20",
-  // },
   {
     id: "personal-assistant-to-ceo",
     title: "Personal Assistant to CEO",
     experience: "2+ Years",
     deadline: "2025-05-30",
   },
+  {
+    id: "product-designer",
+    title: "Product Designer",
+    experience: "3+ Years",
+    deadline: "2025-06-30",
+  },
 ];
+
+export interface Career {
+  id: string;
+  title: string;
+  experience: string;
+  deadline: string;
+}
 
 export interface Career {
   id: string;
